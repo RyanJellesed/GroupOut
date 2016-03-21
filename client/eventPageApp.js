f24f50bc7e00fa5e1371c717cebb56930db605e6
LuckXP <js.sindler@gmail.com>
2016-03-21 09:55:44 -0600

    FACEBOOK_APP_ID changed

---
+++ client/eventPageApp.js
@@ -0,0 +1,47 @@
+var AllEventBox = require('./allEventBox');
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+
+
+var GoEventApp = React.createClass({
+  propTypes: {
+        url: React.PropTypes.string.isRequired
+    },
+    getInitialState: function(){
+        return {
+            events: [],
+            
+        }
+        console.log(events)
+    },
+    
+    loadEventsFromServer: function(){
+        var self=this;
+        $.ajax ({
+            url: this.props.url,
+            method: 'GET'
+        }).done(function(d){
+            console.log(d);
+            self.setState({
+                events: d
+            })
+        })
+    },
+    componentDidMount: function(){
+        console.log("componentDidMount fired");
+        this.loadEventsFromServer()
+    },
+    render: function() {
+    return (
+      <div>
+        <AllEventBox events={this.state.events} />
+      </div>
+
+      )
+    }
+});
+
+
+ReactDOM.render(<GoEventApp url="/api/event/" />,
+  document.getElementById('goCard-app-all'));
