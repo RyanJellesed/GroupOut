753c9e7852b6c50bc107a6fb17e077fbc0763fd8
srs1212 <info@greenspoon.us>
2016-03-23 08:59:52 -0600

    updates to style and about page

--- client/formApp.js
+++ client/formApp.js
@@ -1,4 +1,4 @@
-var React = require('react');
+ var React = require('react');
 var ReactDOM = require('react-dom');
 var injectTapEventPlugin = require("react-tap-event-plugin");
 var DropDowns = require('./drpdowns');
